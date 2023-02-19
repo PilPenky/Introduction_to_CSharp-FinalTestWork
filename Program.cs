aaa9af107c2ac6cb167b5a57a114945190007268
Artur Pilipenko <apg-a@mail.ru>
2023-02-19 15:25:31 +0200

    Добавил код решения поставленной задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[]   {"Twenty five again",
+                                "Twe", "nty", "aga", "in", "=", "45"};
+
+string[] newArray = new string[SearchCount()];
+
+int SearchCount()
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+    if (array[i].Length <= 3)
+        {
+            count+=1;
+        }
+    }
+    return count;
+}
+
+void SearchSmallElement()
+{
+    int tIndex = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+    if (array[i].Length <= 3)
+        {
+            newArray[tIndex] = array[i];
+            tIndex++;
+        }
+    }
+    Console.WriteLine(string.Join(", ", newArray));
+}
+
+SearchSmallElement();
